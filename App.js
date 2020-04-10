b09c9ecdd3341de9ee51674493613e56ae895b82
lhotel <lhotel.man.app@gmail.com>
2020-04-10 11:24:56 -0400

    Set up native elements

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 import Authentication from './components/Authentication';
 
 export default function App() {  
   return(    
-    <View>
-      <Authentication></Authentication>
+    <View style={{ alignItems: 'center'}}>
+      <Text style={{ fontSize: 16 }}>CoachBox</Text>
+      <Text>Authentication</Text>
+      <View>
+        <Text>Coach</Text>
+        <Text>Athlete</Text>
+      </View>
+      <View>
+        <Button title="Coach"/>
+        <Button title="Athlete"/>
+      </View>
+      <Text>Hello Coach!</Text>
+      <Text>Please fill out the form below to get started</Text>
+      <TextInput value="Enter email here..."/>
+      <TextInput value="Enter password here..."/>
+      <Button title="Sign up!"/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+      
+  }
+})
--- components/Authentication.js
+++ components/Authentication.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function Authentication() {
   return(
     <View>
-        <Text>Authentication</Text>
+        <Text>TITLE</Text>
     </View>
   )
 }
+
