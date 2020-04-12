5857b0d811235e8ed56deee36ab20dbc81c58cff
lhotel <lhotel.man.app@gmail.com>
2020-04-12 14:27:38 -0400

    Set up state

--- App.js
+++ App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 import Authentication from './components/Authentication';
 
-export default function App() {  
+export default function App() {
+  const [isCoachSelected, setIsCoachSelected] = React.useState(true);
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
   return(    
     <View style={{ alignItems: 'center'}}>
       <Text style={{ fontSize: 16 }}>CoachBox</Text>
@@ -12,14 +16,22 @@ export default function App() {
         <Text>Athlete</Text>
       </View>
       <View>
-        <Button title="Coach"/>
-        <Button title="Athlete"/>
+        <Button title="Coach" onPress={() => setIsCoachSelected(true)}/>
+        <Button title="Athlete" onPress={() => setIsCoachSelected(false)}/>
       </View>
+      
       <Text>Hello Coach!</Text>
       <Text>Please fill out the form below to get started</Text>
-      <TextInput value="Enter email here..."/>
-      <TextInput value="Enter password here..."/>
+      <Text>Email</Text>
+      <TextInput placeholder={'Enter email here...'} value={email} onChangeText={text => setEmail(text)}/>
+      <Text>Password</Text>
+      <TextInput placeholder={'Enter password here...'} value={password} onChangeText={text => setPassword(text)}/>
       <Button title="Sign up!"/>
+      
+      <Text>-----Debug-----</Text>
+      <Text>IsCoachSelected = {isCoachSelected.toString()}</Text>
+      <Text>Email = {email}</Text>
+      <Text>Password = {password}</Text>    
     </View>
   )
 }
